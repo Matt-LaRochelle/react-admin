d2cf97db017fef1b277c2bd931221cc689a3edaf
matthew <larochelle.matthew@gmail.com>
2023-07-28 15:32:02 -0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
+
+function App() {
+  return (
+    <div>
+      Lama Dev
+      <Home />
+      <List />
+      <Login />
+      <New />
+      <Single />
+    </div>
+  );
+}
+
+export default App;
