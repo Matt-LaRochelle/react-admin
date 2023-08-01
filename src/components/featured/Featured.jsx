42598e71cd9fc28dee41c856d6eafaf2b63def63
matthew <larochelle.matthew@gmail.com>
2023-08-01 09:56:03 -0700

    main chart created

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -20,7 +20,7 @@ const Featured = () => {
                         strokeWidth={5} />
                 </div>
                 <p className="title">Total sales made today</p>
-                <p className="amount">$420</p>
+                <p className="amount">$600</p>
                 <p className="desc">Previous transactions processing. Last payments may not be included.</p>
                 <div className="summary">
                     <div className="item">
@@ -31,14 +31,14 @@ const Featured = () => {
                         </div>
                     </div>
                     <div className="item">
-                        <div className="itemTitle">Target</div>
+                        <div className="itemTitle">Last Week</div>
                         <div className="itemResult positive">
                             <KeyboardArrowUpOutlinedIcon fontSize="small" />
                             <div className="resultAmount">$12.4k</div>
                         </div>
                     </div>
                     <div className="item">
-                        <div className="itemTitle">Target</div>
+                        <div className="itemTitle">Last Month</div>
                         <div className="itemResult negative">
                             <KeyboardArrowDownIcon fontSize="small" />
                             <div className="resultAmount">$12.4k</div>
